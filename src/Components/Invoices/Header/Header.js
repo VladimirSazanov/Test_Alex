10de58a5b48213da5d9942a775a58e563cad69e6
Volodya <vova.sazanov@mail.ru>
2025-07-12 10:13:48 +0300

    Greeting

--- src/Components/Invoices/Header/Header.js
+++ src/Components/Invoices/Header/Header.js
@@ -15,6 +15,7 @@ export default function Header() {
             <div className="ButtonControl">
                 <TableManagement />
             </div>
+            Hello user
         </div>
     )
 }
